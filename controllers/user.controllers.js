89ae3174015e09155367508d7c7eb3a7e14c62dc
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-09-27 16:23:56 +0100

    fix update user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,7 +2,7 @@ import User from '../models/user.model.js';
 import Job from '../models/job.model.js';
 import { StatusCodes } from 'http-status-codes';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multer.middleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId }).select('-password');
@@ -23,8 +23,8 @@ export const updateUser = async (req, res) => {
 
   // If there's an image file in the request body, upload it to Cloudinary
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,15 +1,39 @@
 import multer from 'multer';
+import path from 'path';
+import DataParser from 'datauri/parser.js';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads');
-  },
-  filename: function (req, file, cb) {
-    const fileName = file.originalname;
-    cb(null, fileName);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'public/uploads');
+//   },
+//   filename: function (req, file, cb) {
+//     const fileName = file.originalname;
+//     cb(null, fileName);
+//   }
+// });
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
+const parser = new DataParser();
+
+export const formatImage = file => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer);
+};
+
 export default upload;
+
+// console.log(file) - formatImage parameter
+
+// Output
+
+// {
+//   fieldname: 'avatar',
+//   originalname: 'ArthurOgunfuye.jpeg',
+//   encoding: '7bit',
+//   mimetype: 'image/jpeg',
+//   buffer: <Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 01 00 01 00 00 ff db 00 84 00 06 06 06 06 07 06 07 08 08 07 0a 0b 0a 0b 0a 0f 0e 0c 0c 0e 0f 16 10 11 10 ... 229577 more bytes>,
+//   size: 229627
+// }
